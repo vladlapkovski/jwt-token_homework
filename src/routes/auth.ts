99e99b882f68e1f19ba7a7a5e5017ab4346fd357
vladlapkovski <vlad.lapkofffskii@gmail.com>
2024-01-28 03:20:05 +0300

    fq

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -125,7 +125,7 @@ authRoutes.get('/:me', async (req: Request, res: Response) => {
     if (RegisteredUser) {
         const SendMail = await RegistrationOfUserSocialRepository.RegistrationOfUser(login, password, email)
         if (SendMail) {
-            return res.status(200).send("Input data is accepted. Email with confirmation code will be send to passed email address")
+            return res.status(204).send("Input data is accepted. Email with confirmation code will be send to passed email address")
         } else {
             return res.status(400).send();
         }
